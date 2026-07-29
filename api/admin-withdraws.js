ac35b0a67bfd65eddc44b029dd015c65a0cc2eeb
uzairop6363 <uzairop6363@gmail.com>
2026-07-29 20:58:37 +0500

    Add admin withdraws API endpoint

---
+++ api/admin-withdraws.js
@@ -0,0 +1,92 @@
+import { MongoClient } from "mongodb";
+
+
+const uri = process.env.MONGO_URI;
+
+
+
+export default async function handler(req,res){
+
+
+if(req.method !== "GET"){
+
+
+return res.status(405).json({
+
+success:false,
+
+message:"Method not allowed"
+
+});
+
+
+}
+
+
+
+try{
+
+
+const client = new MongoClient(uri);
+
+
+await client.connect();
+
+
+
+const db =
+client.db("eva_earning");
+
+
+
+const withdraws =
+db.collection("withdraws");
+
+
+
+const allWithdraws =
+await withdraws
+.find({})
+.sort({
+createdAt:-1
+})
+.toArray();
+
+
+
+await client.close();
+
+
+
+res.json({
+
+success:true,
+
+withdraws:allWithdraws
+
+
+});
+
+
+
+}catch(error){
+
+
+console.log(error);
+
+
+
+res.status(500).json({
+
+success:false,
+
+message:error.message
+
+});
+
+
+}
+
+
+
+}
